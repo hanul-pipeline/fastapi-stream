bb903a9a56105f90cb42bd012030f1850754dab1
hooniegit <neivekim76@gmail.com>
2023-11-04 02:38:27 +0900

    231104 🧑‍🏭 feat & fix: main 스크립트 라우터 이름 수정

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from routers import remove_data_router, update_parquet_router
+from routers import update_data_router
 
 app = FastAPI()
-app.include_router(remove_data_router.router)
-app.include_router(update_parquet_router.router)
+app.include_router(update_data_router.router)
 
 if __name__ == "__main__":
     import uvicorn
