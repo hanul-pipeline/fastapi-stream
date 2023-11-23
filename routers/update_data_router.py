9aa5b5df95a7fabbfbab87db34f11ccaccb8b326
hooniegit <neivekim76@gmail.com>
2023-11-23 15:18:59 +0000

    231124 🧑‍🏭 fix: 라우터 location id 재 할당

--- routers/update_data_router.py
+++ routers/update_data_router.py
@@ -9,29 +9,35 @@ router = APIRouter()
 # confirmed
 @router.post('/update/100', response_class=PlainTextResponse)
 async def receive_data_100(data_received: dict):
-    return update_data(data_received)
+    location_id = 7
+    return update_data(data_received, location_id)
 
 # confirmed
 @router.post('/update/200', response_class=PlainTextResponse)
 async def receive_data_200(data_received: dict):
-    return update_data(data_received)
+    location_id = 8
+    return update_data(data_received, location_id)
 
 # confirmed
 @router.post('/update/300', response_class=PlainTextResponse)
 async def receive_data_300(data_received: dict):
-    return update_data(data_received)
+    location_id = 10
+    return update_data(data_received, location_id)
 
 # confirmed
 @router.post('/update/400', response_class=PlainTextResponse)
 async def receive_data_400(data_received: dict):
-    return update_data(data_received)
+    location_id = 11
+    return update_data(data_received, location_id)
 
 # confirmed
 @router.post('/update/500', response_class=PlainTextResponse)
 async def receive_data_500(data_received: dict):
-    return update_data(data_received)
+    location_id = 7
+    return update_data(data_received, location_id)
 
 # confirmed
 @router.post('/update/600', response_class=PlainTextResponse)
 async def receive_data_600(data_received: dict):
-    return update_data(data_received)
+    location_id = 7
+    return update_data(data_received, location_id)
