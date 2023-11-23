ba5f6f38ee360764ea50ae0239274c2fa2909b97
hooniegit <neivekim76@gmail.com>
2023-11-23 14:51:25 +0000

    231123 🧑‍🏭 feat: alert 로직 fastapi-alert 레포지토리로 이관

--- routers/update_data_router.py
+++ routers/update_data_router.py
@@ -31,33 +31,7 @@ async def receive_data_400(data_received: dict):
 async def receive_data_500(data_received: dict):
     return update_data(data_received)
 
-
-# update alert
-@router.post('/alert/7/1', response_class=PlainTextResponse)
-async def alert_7_1(data_received: dict):
-    return update_alert(data_received)
-
-@router.post('/alert/7/2', response_class=PlainTextResponse)
-async def alert_7_2(data_received: dict):
-    return update_alert(data_received)
-
-@router.post('/alert/7/3', response_class=PlainTextResponse)
-async def alert_7_3(data_received: dict):
-    return update_alert(data_received)
-
-@router.post('/alert/7/4', response_class=PlainTextResponse)
-async def alert_7_4(data_received: dict):
-    location_id = 7
-    return update_alert(data_received, location_id)
-
-@router.post('/alert/8/1', response_class=PlainTextResponse)
-async def alert_8_1(data_received: dict):
-    return update_alert(data_received)
-
-@router.post('/alert/10/2', response_class=PlainTextResponse)
-async def alert_10_2(data_received: dict):
-    return update_alert(data_received)
-
-@router.post('/alert/11/2', response_class=PlainTextResponse)
-async def alert_11_2(data_received: dict):
-    return update_alert(data_received)
+# confirmed
+@router.post('/update/600', response_class=PlainTextResponse)
+async def receive_data_600(data_received: dict):
+    return update_data(data_received)
